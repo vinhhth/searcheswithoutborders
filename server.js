40092afa7819e9c875607070e89f646a2dccdacf
Surtai Han <surtai.han@gmail.com>
2015-04-26 17:12:34 -0500

    made server.js less terse. added index.html

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 // run using 'node server.js'
 
+var PORT = 8000;
+var ADDRESS = '127.0.0.1';
+
+// this function specifies how our http server deals with a request
+function processRequest(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello World\n');
+}
+
+
 var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var server = http.createServer();
+
+server.listen(PORT, ADDRESS);
+
+// tell our http server how to deal with a request (e.g. using processRequest)
+server.addListener("request", processRequest);
+
+console.log("Server running at http://" + ADDRESS + ":" + PORT + "/");
