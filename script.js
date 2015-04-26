5e3acf14466d2d97699b1d21a8f4e34912dc60fd
Surtai Han <surtai.han@gmail.com>
2015-04-26 17:31:07 -0500

    added google-hosted jquery and added .ready()

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // this is the javascript for the HTML page
 
-alert("front end javascript call");
+// code put here will run before the document is actually loaded
+
+// this function is called once all the HTML is loaded
+// this is the "main" method if you will
+$(document).ready(function() {
+	alert("document is ready. front end javascript call.");
+
+});
