880328260ce5950597471e38e251e526b18dce93
Varun Thangavelu <tvarun2013@gmail.com>
2015-04-27 01:34:01 -0500

    working

--- data_server.js
+++ data_server.js
@@ -12,24 +12,24 @@ var cheerio = require('cheerio');
 
 // this function specifies how our http server deals with a request
 function processRequest(request, response) {
-	var pathname = url.parse(request.url).pathname;
+    var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-	// just ignore this bullshit. DO NOT delete it.
-	response.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
-	response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-	response.setHeader('Access-Control-Allow-Credentials', true);
+    // just ignore this bullshit. DO NOT delete it.
+    response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    response.setHeader('Access-Control-Allow-Credentials', true);
 
-	response.writeHead(200);
+    response.writeHead(200);
 
-	var raw_query = pathname.substring(1, pathname.length);
+    var raw_query = pathname.substring(1, pathname.length);
 
-	var query = raw_query.replace(/%20/g, " ");
+    var query = raw_query.replace(/%20/g, " ");
 
-	// response.write("You submitted the query: " + query);
+    // response.write("You submitted the query: " + query);
 
-	http.get({
+    http.get({
         host: 'www.google.com',
         path: '/search?q=' + raw_query
     }, function(res) {
@@ -42,32 +42,34 @@ function processRequest(request, response) {
 
             // Data reception is done, do whatever with it!
             //var parsed = JSON.parse(body);
-			var $ = cheerio.load(body);
-			var actualData = $("#res")[0];
-			$ = cheerio.load(actualData);
-			//$("#imagebox_bigimages").remove();
-			//console.log(actualData);
+            var $ = cheerio.load(body);
+            var actualData = $("#res")[0];
+            $ = cheerio.load(actualData);
+            //$("#imagebox_bigimages").remove();
+            //console.log(actualData);
 
-			$('a').each(function(index) {
-				var url = this.attribs.href;
-				if(url.substring(0,7) == "/url?q=") {
-					var index = url.indexOf("&sa");
-					var cleanedURL = url.substring(7,index);
-					cleanedURL = cleanedURL.replace(/%3F/g, "?");
-					cleanedURL = cleanedURL.replace(/%3D/g, "=");
-					cleanedURL = cleanedURL.replace(/%23/g, "#");
-					cleanedURL = cleanedURL.replace(/%26/g, "&");
+            $('a').each(function(index) {
+                var url = this.attribs.href;
+                if (url.substring(0, 7) == "/url?q=") {
+                    var index = url.indexOf("&sa");
+                    var cleanedURL = url.substring(7, index);
+                    cleanedURL = cleanedURL.replace(/%3F/g, "?");
+                    cleanedURL = cleanedURL.replace(/%3D/g, "=");
+                    cleanedURL = cleanedURL.replace(/%23/g, "#");
+                    cleanedURL = cleanedURL.replace(/%26/g, "&");
 
-					if(cleanedURL.indexOf("webcache.googleusercontent.com") == -1 && cleanedURL.indexOf("/settings/ads/preferences") == -1)
-						response.write(cleanedURL + "<br>");
-				}
-				//response.write(this.attribs.href + "<br>");
-			});
-			//console.log(body);
+                    if (cleanedURL.indexOf("webcache.googleusercontent.com") == -1 && cleanedURL.indexOf("/settings/ads/preferences") == -1)
+                        response.write(cleanedURL + "<br>");
+                }
+                //response.write(this.attribs.href + "<br>");
+            });
+            //console.log(body);
             //response.write(body);
-			response.end();
+            response.end();
         });
-    });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+    });;
 
 }
 
